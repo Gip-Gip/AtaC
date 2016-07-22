088fb114d6d5ca46dd474537fd772db6c42040e5
Charles Thompson <cmt.16.yes@gmail.com>
2016-07-21 22:59:31 -0400

    Uploaded

---
+++ atar/messages.h
@@ -0,0 +1,18 @@
+#define MSG_SPLASH "\n\
+AtaR I.MMXVI rev 0. Run with --help for a quick guide\n\n"
+#define MSG_FILEEXISTS "\n\"%s\" already exists! Run with -y to overwrite\n\n",\
+                       argv[argn]
+#define MSG_PERROR "FATAL ERROR"
+#define MSG_LICENSE "Copyright (c) 2016, Charles Thompson\n\
+\n\
+Permission to use, copy, modify, and/or distribute this software for any\n\
+purpose with or without fee is hereby granted, provided that the above\n\
+copyright notice and this permission notice appear in all copies.\n\
+\n\
+THE SOFTWARE IS PROVIDED \"AS IS\" AND THE AUTHOR DISCLAIMS ALL WARRANTIES\n\
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF\n\
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR\n\
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES\n\
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN\n\
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF\n\
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.\n"
